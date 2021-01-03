aa5cdc67392557cd414d4941cd3c77e1d86456b1
studious79 <studious79@gmail.com>
2021-01-02 20:19:19 -0800

    Include project folder

---
+++ expandingcards/script.js
@@ -0,0 +1,20 @@
+//bring in panels
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach( (panel) => {
+
+    panel.addEventListener('click', () => {
+
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+
+    panels.forEach( (panel) => {
+
+        panel.classList.remove('active')
+    })
+}
+
